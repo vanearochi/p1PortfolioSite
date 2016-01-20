8da7aa3ad7775544cc1e2cedfcbfb5824a52dfa3
Vanessa Arochi <vanearochi@gmail.com>
2016-01-20 15:11:10 -0800

    Finished Project, changed variable names, cleaned things that weren't useful, documentation comments

--- media/js/p1Portfolio.js
+++ media/js/p1Portfolio.js
@@ -1,17 +1,18 @@
 $(document).ready(function(){
+
 	console.log("Welcome to Portfolio Vane");
 
-
-	var hide_all_but_one = function(elements, attribute_name, attribute_value ){
+	/**Function that pass trough all the elements array indexes so it can hide or show an element depending
+	on the comparition between the attribute value and the curent value*/
+	var hideAllButOne = function(elements, attributeName, attributeValue ){
 		$(elements).each(function(index){
-			var current_element = $(elements[index]);
-			var current_value = current_element.attr(attribute_name);
-			
-			if(current_value == attribute_value){
-				current_element.show();
-			} 
+			var currentElement = $(elements[index]);
+			var currentValue = currentElement.attr(attributeName);
+			if(currentValue == attributeValue){
+				currentElement.show();
+			}
 			else{
-				current_element.hide();
+				currentElement.hide();
 			}
 		});
 	}
@@ -19,44 +20,19 @@ $(document).ready(function(){
 	console.log("Testing... 1,2,3...");
 
 	var sections = $(".central-section > section");
+	/** hideAllButOne function is called so it can show the home section when is opened or refreshed*/
+	hideAllButOne(sections, 'data-tab', 'home');
 
-	hide_all_but_one(sections, 'data-tab', 'home');
-		
 
-		
-
+	/** Function for navigation functionality, takes the id of the clicked li and takes an array of
+	the sections of the html so it can pass them as a parameter when at the end calls the changeAllbutOne function */
 	$(".navigation li").on("click", function(){
-		console.log("1");
 
-		var selected_tab_name = $(this).attr('id')
+		var selectedTabName = $(this).attr('id')
 		var sections = $(".central-section > section");
+		hideAllButOne(sections, 'data-tab', selectedTabName);
 
-		hide_all_but_one(sections, 'data-tab', selected_tab_name);
 
-		// $("li").removeClass("active")
-		// $(this).addClass("active");
-		// liClass= $(this).attr('id');
-		// console.log(liClass)
-		// if(liClass==="about-me"){
-		// 	$(".central-image").fadeOut();
-	 //  		$(".about-me").show();
-	 //  		$(".featured-work-section").fadeOut();
-	 //  		$(".contact-me").hide();
-	 //  	}
-	 //  	else if(liClass==="home"){
-	 //  		$(".central-image").show();
-	 //  		$(".contact-me").hide();
-	 //  		$(".featured-work-section").show();
-	 //  		$(".about-me").hide();
-	 //  	}
-	 //  	else if(liClass==="contact-me"){
-	 //  		console.log(2)
-	 //  		$(".central-image").fadeOut();
-	 //  		$(".contact-me").show();
-	 //  		$(".featured-work-section").fadeOut();
-	 //  		$(".about-me").hide();
-	 //  	}
-	  	
-	})		
+	})
 
 });
